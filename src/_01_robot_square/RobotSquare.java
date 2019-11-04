96e9b31ccaf9dcf7d6bd95b733a5099bfd438838
Kevin Lee <kevindlee@gmail.com>
2019-11-03 20:14:46 -0800

    Complete module 0 and 1

--- src/_01_robot_square/RobotSquare.java
+++ src/_01_robot_square/RobotSquare.java
@@ -12,21 +12,30 @@ public class RobotSquare {
     public static void main(String[] args) throws Exception {
  
     	// 1. Make a new Robot
+        Robot rob = new Robot();
+        if ( rob == null ) {
+            throw new Exception();
+        }
 
 
         // 3. Put the robot's pen down
+        rob.penDown();
 
 
         // 6. Make the robot move as fast as possible
+        rob.setSpeed(100);
 
 
         // 5. Do everything below here 4 times
+        for (int i = 0; i < 4; i++) {
+            //         2. Move your robot 200 pixels
+            rob.move(200);
+
+            //         4. Turn the robot 90 degrees to the right (90 degrees)
+            rob.turn(90);
+        }
 
 
-        //         2. Move your robot 200 pixels
-
-
-        //         4. Turn the robot 90 degrees to the right (90 degrees)
 
 
     }
